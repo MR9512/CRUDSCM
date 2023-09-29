d9d279a3bbdb893b3ee3fe2b21818dce5265c842
Marco Rodriguez <mromero.10@hotmail.com>
2023-09-29 13:42:46 -0600

    MARR: Asignación pagina 404 como metodo

--- routers/router.php
+++ routers/router.php
@@ -5,40 +5,43 @@ class router{
     private $method;
 
       public function __construct(){
+        $this->bulmaCSS = "../assets/css/bulma.min.css"; 
         $this->matchRoute();
       }
-      
-      public function matchRoute(){
-          $url = explode("/", URL);
-          $this->controller = $url[1];
-          $metodo = explode("?", $url[2]);
-          if ($url[2] == "") {
-              $this->method = "dashboard";
-          } else {
-              $this->method = $metodo[0];
+        
+          public function show404Error() {
+            require_once("views/templates/header.php");
+            require_once("views/404/404.php");
+            require_once("views/templates/footer.php");
+            exit;
           }
-          $this->controller = $this->controller . "Controller";
-        if (file_exists("Controllers/" . $this->controller . ".php")) {
-            require_once "Controllers/" . $this->controller . ".php";
-        } else {
-            $this->show404Error();
-        }
-      }
 
-      public function run(){
-        $controller = new $this->controller();
-        if (method_exists($controller, $this->method)) {
-            $method = $this->method;
-            $controller->$method();
-        } else {
-            $this->show404Error();
+         public function matchRoute(){
+            $url = explode("/", URL);
+            $this->controller = $url[1];
+            $metodo = explode("?", $url[2]);
+               if ($url[2] == "") {
+                  $this->method = "dashboard";
+               } else {
+                  $this->method = $metodo[0];
+               }
+            $this->controller = $this->controller . "Controller";
+            if (file_exists("Controllers/" . $this->controller . ".php")) {
+                require_once "Controllers/" . $this->controller . ".php";
+            } else {
+                $this->show404Error();
+            }
         }
-    }
 
-    private function show404Error() {
-        echo "Error 404 - Página no encontrada";
-        exit;
-    }
+         public function run(){
+            $controller = new $this->controller();
+               if (method_exists($controller, $this->method)) {
+                  $method = $this->method;
+                  $controller->$method();
+               } else {
+                  $this->show404Error();
+               }
+          }
  
 }
 ?>
